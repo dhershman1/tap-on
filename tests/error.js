3bec72521dad0e6ad00ab38d2fb097994ff1b4e7
dhershman1 <dustinh17@gmail.com>
2024-01-28 15:40:22 -0500

    Expand error testing

--- tests/error.js
+++ tests/error.js
@@ -3,11 +3,16 @@ import test from 'tape'
 /* eslint-disable */
 
 test('good test', t => {
-  t.ok(true);
-  t.end();
-});
+  t.ok(true)
+  t.end()
+})
+
+test('good test 2', t => {
+  t.ok(1)
+  t.end()
+})
 
 test('bad test', t => {
-  t.ok(undefined.length);
-  t.end();
-});
+  t.ok(undefined.length)
+  t.end()
+})
